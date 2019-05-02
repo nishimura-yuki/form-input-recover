05a267b11d8df0507fc630450ad81ba487cfb427
greendiver234@gmail.com <greendiver234@gmail.com>
2019-05-02 11:49:24 +0900

    teardown.js を追加し忘れていたので追加。

---
+++ test/e2e/config/teardown.js
@@ -0,0 +1,13 @@
+const os = require('os');
+const rimraf = require('rimraf');
+const path = require('path');
+
+const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
+module.exports = async function() {
+  // close the browser instance
+  await global.__BROWSER_GLOBAL__.close();
+  await global.__SERVER_GLOBAL__.close();
+
+  // clean-up the wsEndpoint file
+  rimraf.sync(DIR);
+};
